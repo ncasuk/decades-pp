f9320e09c4b3c4cbd6d0923f55ee53ee3b5f8b60
Dave Tiddeman <dave.tiddeman@metoffice.gov.uk>
2013-11-25 10:05:29 +0000

    Changed setup.py to hopefully make installing work better

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from distutils.core import setup
 from distutils.command.build import build as distbuild
 
-class prebuild(distbuild):
+class fortbuild(distbuild):
     def run(self):
-        """ make fortran modules before build """
+        """ make fortran modules after build """
+        distbuild.run(self)
         import os,subprocess
         d=os.getcwd()
-        os.chdir('ppodd/pod/fortran_modules')
+        print self.build_lib
+        os.chdir(os.path.join(self.build_lib,'ppodd/pod/fortran_modules'))
         subprocess.call('make')
         os.chdir(d)
-        distbuild.run(self)
 
 
 setup(name = "ppodd",
-    version = "001",
+    version = "002",
     description = "Post Processing of Decades Data",
     author = "Dave Tiddeman",
     author_email = "dave.tiddeman@metoffice.gov.uk",
     url = "http://www.faam.ac.uk",
     packages = ['ppodd','ppodd.gui','ppodd.pod'],
-    package_data = {'ppodd.pod' : ["fortran_modules/*.for","fortran_modules/makefile"] },
+    package_data = {'ppodd.pod' : ["runmod.so","fortran_modules/*.for","fortran_modules/makefile"] },
     include_package_data=True,
     scripts = ["PPODD"],
     long_description = """Post-processing of decades data."""
-    #,cmdclass={'build': prebuild}   
+    ,cmdclass={'build': fortbuild}   
 ) 
