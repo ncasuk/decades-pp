06286397f611ff5adae9e9abcf80417f17524b15
Dave Tiddeman <dave.tiddeman@metoffice.gov.uk>
2014-02-12 16:30:49 +0000

    Updated Thermistor temperature calculation for Total Water Sample Temp - Needs constants file changing to have an effect. New values .. CALTSAM   11.02757718 3956.07906064 1932.77875494 7162.35330738

--- ppodd/pod/p_twc_rio.py
+++ ppodd/pod/p_twc_rio.py
@@ -23,6 +23,25 @@ class twc_rio(cal_base):
         #self.name='TWC'
         self.version=1.00
         cal_base.__init__(self,dataset)
+        
+    def thermistor(self,data,A,B,C,D):
+        # A combination of a simplified thermistor equation R=Nexp(B/T)
+        # and the parallel resistance Rout=R*Rp/(R+Rp)
+        # and linear voltage from Rout,  Decades=m*Rout+c
+        # gives something like T=B/(ln(R)-ln(N)) , where  R=Rp*(Decades-c)/(c+m*Rp-Decades)
+        # Constants
+        # A is a combination of N, m and Rp..
+        # B is the Beta value of the Thermistor
+        # C is related to the decades value displayed for a short circuit ( but not quite... ) 
+        # D is minus the decades value for an open circuit
+        # Calculated using scipy.optimize.curve_fit(thermistor,decades,temp,p0=[11,4000,2000,8000]) 
+        # p0 has to be a reasonable first guess - particularly D must be greater than the max negative decades data value.    
+        #
+        return B/(np.log(C-data)-np.log(data+D)+A)
+        
+    def fit_thermistor(self,data,temps):
+        import scipy.optimize
+        return scipy.optimize.curve_fit(self.thermistor,data,temps,p0=[11,4000,2000,8000])
                     
     def process(self):
         """
@@ -36,7 +55,8 @@ class twc_rio(cal_base):
         calisrc - -0.4mA < I < -1.1mA                             0.05mA/s
         !c_twc     Lyman alpha total water - temps in K, currents in A, TWC decades refit 22/10/2013
         CALTNOS   1.2614E-16 -1.8668E-12  1.2704E-08  9.4262E-03  3.1654E+02
-        CALTSAM   2.5406E-17  3.5253E-13  2.1300E-09  7.1707E-06  2.7927E-02  4.0343E+02
+        CALTSAM   11.02757718 3956.07906064 1932.77875494 7162.35330738
+        !CALTSAM   2.5406E-17  3.5253E-13  2.1300E-09  7.1707E-06  2.7927E-02  4.0343E+02
         CALTAMB   2.9951E-13  9.3288E-10 -4.0779E-06  1.6016E-02  2.7390E+02
         CALTSRC   2.8700E-18 -1.2794E-14  2.8480E-11  2.2585E-10  9.5178E-03  3.7298E+02
         CALHTR1   9.7752E-04  0.0000E+00
@@ -65,7 +85,12 @@ class twc_rio(cal_base):
         flags64=np.zeros(times.at_frequency(64).shape,dtype='u1')
         self.outputs[0].data=flagged_data(self.inputs['TWCDAT_twc_detector'].data,times,flags64)
         self.outputs[1].data=flagged_data(np.polyval(caltnos,self.inputs['TWCDAT_twc_nose_temp'].data),times,flags.copy())
-        self.outputs[2].data=flagged_data(np.polyval(caltsam,self.inputs['TWCDAT_twc_samp_temp'].data),times,flags.copy())
+        if len(caltsam)==4:
+            # Fit to sample temp using thermistor equation - should be 4 constants
+            self.outputs[2].data=flagged_data(self.thermistor(self.inputs['TWCDAT_twc_samp_temp'].data,*caltsam),times,flags.copy())            
+        else:
+            # Old polynomial fit for sample temp
+            self.outputs[2].data=flagged_data(np.polyval(caltsam,self.inputs['TWCDAT_twc_samp_temp'].data),times,flags.copy())
         self.outputs[3].data=flagged_data(np.polyval(caltamb,self.inputs['TWCDAT_twc_amb_temp'].data),times,flags.copy())
         self.outputs[4].data=flagged_data(np.polyval(caltsrc,self.inputs['TWCDAT_twc_srce_temp'].data),times,flags.copy())
         self.outputs[5].data=flagged_data(np.polyval(calhtr1,self.inputs['TWCDAT_twc_evap1'].data),times,flags.copy())
