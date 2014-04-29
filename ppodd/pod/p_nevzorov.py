e5527338e4102c3ba1d0fafeed9aec8fcf014b3e
Dave Tiddeman <dave.tiddeman@metoffice.gov.uk>
2014-04-29 09:25:17 +0100

    Nevzorov code now takes constants from flight constants in the form...
    !c_nevz    Nevzorov
    CLWCIREF    -6.815197E-02   3.292018E-04  5.0e-01
    CLWCVREF    -6.823619E-02   3.292050E-04  2.0
    CLWCICOL    -6.812802E-02   3.292026E-04  5.0e-01
    CLWCVCOL    -6.812821E-02   3.292026E-04  2.0
    CTWCIREF    -6.835050E-02   3.292064E-04  5.0e-01
    CTWCVREF    -6.822893E-02   3.292051E-04  2.0
    CTWCICOL    -6.825762E-02   3.292069E-04  5.0e-01
    CTWCVCOL    -6.826676E-02   3.292077E-04  2.0
    CALNVLWC    0.7 0.3e-4
    CALNVTWC    1.65 0.5e-4
    CALNVL      2589.0

--- ppodd/pod/p_nevzorov.py
+++ ppodd/pod/p_nevzorov.py
@@ -11,7 +11,12 @@ class nevzorov(cal_base):
                             'CORCON_nv_twc_icol',
                             'CORCON_nv_twc_vref',
                             'CORCON_nv_twc_iref',
-                            'TAS']
+                            'TAS',
+                            'CLWCIREF','CLWCVREF','CLWCICOL','CLWCVCOL',
+                            'CTWCIREF','CTWCVREF','CTWCICOL','CTWCVCOL',
+                            'CALNVLWC',
+                            'CALNVTWC',
+                            'CALNVL']
         self.outputs=[parameter('NV_LWC_U',units='gram m-3',frequency=64,number=602,long_name='Uncorrected liquid water content from the Nevzorov probe')
                      ,parameter('NV_TWC_U',units='gram m-3',frequency=64,number=605,long_name='Uncorrected total condensed water content from the Nevzorov probe.')]
 
@@ -19,56 +24,31 @@ class nevzorov(cal_base):
         cal_base.__init__(self,dataset)
         
     def process(self):
-        CALNVLWC=[0.7,0.3e-4,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2]
-        CALNVTWC=[1.65,0.5e-4,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2]
-        CALNVL=[2589.0]
+        #CALNVLWC=[0.7,0.3e-4,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2]
+        #CALNVTWC=[1.65,0.5e-4,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2,3.29E-4,-6.82E-2]
+        #CALNVL=[2589.0]
         t=self.dataset.matchtimes(self.input_names)
-        licol=self.dataset['CORCON_nv_lwc_icol'].ismatch(t)   
-        lvcol=self.dataset['CORCON_nv_lwc_vcol'].ismatch(t)     
-        liref=self.dataset['CORCON_nv_lwc_iref'].ismatch(t)     
-        lvref=self.dataset['CORCON_nv_lwc_vref'].ismatch(t) 
-        ticol=self.dataset['CORCON_nv_twc_icol'].ismatch(t)   
-        tvcol=self.dataset['CORCON_nv_twc_vcol'].ismatch(t)     
-        tiref=self.dataset['CORCON_nv_twc_iref'].ismatch(t)     
-        tvref=self.dataset['CORCON_nv_twc_vref'].ismatch(t) 
-        AREA_TWC=CALNVTWC[0]
-        AREA_LWC=CALNVLWC[0]
-        K_TWC=CALNVTWC[1]
-        K_LWC=CALNVLWC[1]
-        L_WC=CALNVL[0]
-        #calibrate raw data
-        lwc_col_i = CALNVLWC[2]*licol + CALNVLWC[3]
-        lwc_col_v = CALNVLWC[4]*lvcol + CALNVLWC[5]
-        lwc_ref_i = CALNVLWC[6]*liref + CALNVLWC[7]
-        lwc_ref_v = CALNVLWC[8]*lvref + CALNVLWC[9]
-        #Calculate derived parameters.
-        #
-        #Sensor power (J/s).
-        lwc_col_p=lwc_col_i*lwc_col_v
-        lwc_ref_p=lwc_ref_i*lwc_ref_v
-        times=licol.times2d.ravel()
-        sh=licol.shape
- 
+        insts=['lwc','twc']
+        measurements=['icol','vcol','iref','vref']
+        cal={}
+        times=self.dataset['CORCON_nv_lwc_vcol'].ismatch(t).times2d
+        sh=times.shape
+        nvl=self.dataset['CALNVL'][0]
         tas=self.dataset['TAS'].ismatch(t).ravel()
         tas.interp1d()
         tas=tas.interpolated(times).reshape(sh)
-        lwc_p=lwc_col_p-K_LWC*lwc_ref_p  
-        lwc_q=lwc_p/(tas*AREA_LWC*L_WC)
-        
-        twc_col_i = CALNVTWC[2]*ticol + CALNVTWC[3]
-        twc_col_v = CALNVTWC[4]*tvcol + CALNVTWC[5]
-        twc_ref_i = CALNVTWC[6]*tiref + CALNVTWC[7]
-        twc_ref_v = CALNVTWC[8]*tvref + CALNVTWC[9]
-        #Calculate derived parameters.
-        #
-        #Sensor power (J/s).
-        twc_col_p=twc_col_i*twc_col_v
-        twc_ref_p=twc_ref_i*twc_ref_v
- 
-        twc_p=twc_col_p-K_LWC*twc_ref_p  
-        twc_q=twc_p/(tas*AREA_TWC*L_WC)
-        self.outputs[0].data=lwc_q
-        self.outputs[1].data=twc_q
-        
-
+        for n,i in enumerate(insts):
+            #For each instrument (i)
+            area=self.dataset[('calnv%s' % i).upper()][0]
+            K=self.dataset[('calnv%s' % i).upper()][1]
+            for m in measurements:
+                raw=self.dataset['CORCON_nv_%s_%s' % (i,m)].ismatch(t)
+                cons=self.dataset[('c%s%s' % (i,m)).upper()]
+                #Calibrate to volts or current
+                cal['%s%s' % (i,m)]=(cons[0]+cons[1]*raw)*cons[2]
+            #Sensor power (J/s).
+            col_p=cal['%sicol' % i]*cal['%svcol' % i]  # V*I
+            ref_p=cal['%siref' % i]*cal['%svref' % i]
+            p=col_p-K*ref_p
+            self.outputs[n].data=p/(tas*area*nvl)
 
