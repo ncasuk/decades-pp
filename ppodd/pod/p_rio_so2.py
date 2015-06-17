d8ae6478d1ccc14b24b31a5aee0312526714fdd5
Axel Wellpott@dacru <axll@faam.ac.uk>
2015-06-17 15:48:29 +0100

    Calibration coefficient is now applied to SO2 concentration

--- ppodd/pod/p_rio_so2.py
+++ ppodd/pod/p_rio_so2.py
@@ -11,7 +11,7 @@ class rio_so2_mixingratio(cal_base):
     """
 
     def __init__(self,dataset):
-        self.input_names=['CHTSOO_conc', 'CHTSOO_flow', 'CHTSOO_flags', 'CHTSOO_utc_time']
+        self.input_names=['CALSO2', 'CHTSOO_conc', 'CHTSOO_flow', 'CHTSOO_flags', 'CHTSOO_utc_time']
         self.outputs=[parameter('SO2_TECO',
                                 units='ppb',
                                 frequency=1,
@@ -25,12 +25,10 @@ class rio_so2_mixingratio(cal_base):
         match=self.dataset.matchtimes(self.input_names)
         so2_mr=self.dataset['CHTSOO_conc'].data.ismatch(match)
 
+        #apply calibration coefficients
+        so2_mr=so2_mr*self.dataset['CALSO2'].data[0]+ self.dataset['CALSO2'].data[1]
+
         flag=np.array([0]*so2_mr.size, dtype=np.int8) # initialize empty flag array, with all flags set to 0
-        #flag[co_mr<-10]=3         # flag very negative co_mr as 3
-        #flag[cal_status==1]=3     # flag calibration data points
-        #flag[calpress>3.2]=3      # flag when calibration gas pressure is increased
-
         so2_teco=flagged_data(so2_mr, match, flag)
-
         self.outputs[0].data=so2_teco
 
