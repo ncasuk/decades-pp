20b0ad344a6cc28702d665ee2c57e2e7ffc8f0cc
Axel Wellpott@dacru <axll@faam.ac.uk>
2016-04-27 16:11:23 +0100

    CPC module debugged with the first CPC data set from b953

--- ppodd/pod/p_rio_cpc.py
+++ ppodd/pod/p_rio_cpc.py
@@ -10,41 +10,41 @@ class rio_cpc(cal_base):
     """
 
     def __init__(self,dataset):
-        self.input_names=['CPC37801_utc_time',
-                          'CPC37801_counts',
-                          'CPC37801_sample_flow',
-                          'CPC37801_total_flow',
-                          'CPC37801_sheath_flow',
-                          'CPC37801_pressure',
-                          'CPC37801_saturator_temp',
-                          'CPC37801_growth_tube_temp',
-                          'CPC37801_optics_temp']
+        self.input_names=['CPC378_utc_time',
+                          'CPC378_counts',
+                          'CPC378_sample_flow',
+                          'CPC378_total_flow',
+                          'CPC378_sheath_flow',
+                          'CPC378_pressure',
+                          'CPC378_saturator_temp',
+                          'CPC378_growth_tube_temp',
+                          'CPC378_optics_temp']
                           
         self.outputs=[parameter('CPC_CNTS',
                                 units='#',
                                 frequency=10,
-                                long_name='Condensation Particle Counter measure by the TSI 3786')]
+                                long_name='Condensation Particle Counts measured by the TSI 3786'),]
         
         self.version=1.00
         cal_base.__init__(self,dataset)
 
     def process(self):
         match=self.dataset.matchtimes(self.input_names)
-        cpc_cnts=self.dataset['CPC37801_counts'].data.match()
-        cpc_sample_flow=self.dataset['CPC37801_sample_flow'].data.match()
-        cpc_total_flow=self.dataset['CPC37801_total_flow'].data.match()
-        cpc_sheath_flow=self.dataset['CPC37801_sheath_flow'].data.match()
-        cpc_pressure=self.dataset['CPC37801_pressure'].data.match()
-        cpc_saturator_temp=self.dataset['CPC37801_saturator_temp'].data.match()
-        cpc_growth_tube_temp=self.dataset['CPC37801_growth_tube_temp'].data.match()
-        cpc_optics_temp=self.dataset['CPC37801_optics_temp'].data.match()
+        cpc_cnts=self.dataset['CPC378_counts'].ismatch(match) 
+        cpc_sample_flow=self.dataset['CPC378_sample_flow'].ismatch(match) 
+        cpc_total_flow=self.dataset['CPC378_total_flow'].ismatch(match) 
+        cpc_sheath_flow=self.dataset['CPC378_sheath_flow'].ismatch(match) 
+        cpc_pressure=self.dataset['CPC378_pressure'].ismatch(match) 
+        cpc_saturator_temp=self.dataset['CPC378_saturator_temp'].ismatch(match) 
+        cpc_growth_tube_temp=self.dataset['CPC378_growth_tube_temp'].ismatch(match) 
+        cpc_optics_temp=self.dataset['CPC378_optics_temp'].ismatch(match) 
         
         # FLAG 0 – Data OK
         # FLAG 1 = Saturator temperature more than 6 degrees C, Growth or Optics Temp more/less than 10% from prescribed value
         # FLAG 2 = Aerosol (Sample) flow more/less than 10% from prescribed value
         # FLAG 3 = Sheath flow more/less than 10% from prescribed value
         
-        flag=np.array([0]*cpc_cnts.size, dtype=np.int8) # initialize empty flag array, with all flags set to 0
+        flag=np.array([0]*cpc_cnts.size, dtype=np.int8).reshape(cpc_cnts.shape) # initialize empty flag array, with all flags set to 0
         
         flag[cpc_saturator_temp > 6.0]=1
         flag[(cpc_growth_tube_temp < 40.5) | (cpc_growth_tube_temp > 49.5)]=1
@@ -52,7 +52,6 @@ class rio_cpc(cal_base):
         flag[(cpc_sample_flow < 0.27) | (cpc_sample_flow > 0.33)]=2
         flag[(cpc_sheath_flow < 0.27) | (cpc_sheath_flow > 0.33)]=3
         
-        cpc_cnts=flagged_data(cpc_cnts, match, flag)
-        self.outputs[0].data=cpc_cnts
-        return
+        self.outputs[0].data=flagged_data(cpc_cnts, match, flag)
+        
 
