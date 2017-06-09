f44dbc00c067355a2a56db900c56c0890beb6f60
Axel Wellpott @ obelix <axel@obelix>
2017-06-09 08:14:18 +0100

    version tag added

--- ppodd/__init__.py
+++ ppodd/__init__.py
@@ -1,19 +1,21 @@
-"""**ppodd** has been written as the standard post processing for core data from the `FAAM <http://www.faam.ac.uk/>`_ BAe146 recorded with the `DECADES <http://www.faam.ac.uk/index.php/new-projects/196-projectspace/450-integration-decades>`_ data acquisition system, installed from October 2013. It is also compatible with the majority of data recorded with the FAAM aircraft since 2004, but not every legacy data type is supported (yet).
+"""
+
+**ppodd** has been written as the standard post processing for core data from the `FAAM <http://www.faam.ac.uk/>`_ BAe146 recorded with the `DECADES <http://www.faam.ac.uk/index.php/new-projects/196-projectspace/450-integration-decades>`_ data acquisition system, installed from October 2013. It is also compatible with the majority of data recorded with the FAAM aircraft since 2004, but not every legacy data type is supported (yet).
 
 There is a programming interface, a command line (decades-pp), and a graphical user interface (PPODD).
 
 It is intended to be a flexible and modular system. 
 
-@author Dave Tiddeman
 """ 
+
 import logging
 import os.path
 import time
 logging.basicConfig()
 
-logger=logging.getLogger('PPODD')
+logger= logging.getLogger('PPODD')
 logger.setLevel(logging.DEBUG)
-version='v004'
+version = '1.0'
 
 def logfile(filename=None):
     if(not filename):
