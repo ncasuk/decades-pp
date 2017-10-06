8d670f059a018cf94c34910bc61ab33b6d3a936a
Graeme Nott <graeme.nott@faam.ac.uk>
2017-10-06 15:18:16 +0100

    Initial import

---
+++ ppodd/pod/p_calc_sea.py
@@ -0,0 +1,20 @@
+
+import ppodd
+from ppodd.core import *
+
+
+
+class calc_sea(cal_base):
+    """
+    Class to calculate water content from the raw data supplied by p_read_sea.
+
+
+    Attributes:
+        dataset (?)
+    """
+
+    def __init__(self,dataset):
+        """
+
+
+        """
